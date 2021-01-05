8654e62dd404b00dcf6b83de295fe9003c36a586
Muhammad-MO <muhdmahmood@gmail.com>
2021-01-05 16:10:09 +0000

    pyhon demo and revision

---
+++ revision.py
@@ -0,0 +1,9 @@
+
+
+print("2021 New Year Resolution BMI calculator")
+weight = input("please enter your weight in kg:")
+height = input("please enter your height in meters:")
+weight = float(weight)
+height = float(height)
+bmi = weight / (height * height)
+print("your bmi is ", bmi)
